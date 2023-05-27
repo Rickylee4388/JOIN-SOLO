dfad2bdeed92e8e8470aab626fa12ca1334bee7c
PhilippKlinger <klinger.breuberg@web.de>
2023-05-27 19:22:59 +1200

    added simple js to main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 function init() {
     includeHTML();
 }
+
+function renderSummary() {
+    contentSection.innerHTML = '<h2>Summary</h2>';
+}
+
+function renderBoard() {
+    contentSection.innerHTML = '<h2>Board</h2>';
+}
+
+function renderAddTask() {
+    contentSection.innerHTML = '<h2>Add Task</h2>';
+}
+
+function renderContacts() {
+    contentSection.innerHTML = generateContactsHTML();
+}
--- templates/js/contactsTemplates.js
+++ templates/js/contactsTemplates.js
@@ -1,9 +1,9 @@
 function initContacts() {
     includeHTML();
-    renderContentSection();
+    renderContentSectionContacts();
 }
 
-function renderContentSection() {
+function renderContentSectionContacts() {
     contentSection.innerHTML = generateContactsHTML();
 }
 
