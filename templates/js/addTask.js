97f111531647809670848b9cf36aa68b020d5915
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-06-01 18:05:06 +0200

    added clear function in add task page

---
+++ templates/js/addTask.js
@@ -0,0 +1,6 @@
+function clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('subtasks').value = '';
+}
