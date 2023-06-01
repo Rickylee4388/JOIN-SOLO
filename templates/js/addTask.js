e2543cf38975b71d407568fa758fdb394a8ff764
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-06-01 19:57:41 +0200

    improved clear function for add task page

--- templates/js/addTask.js
+++ templates/js/addTask.js
@@ -3,4 +3,33 @@ function clearFields() {
     document.getElementById('description').value = '';
     document.getElementById('date').value = '';
     document.getElementById('subtasks').value = '';
+    clearPrioButtons();
+}
+
+
+function clearPrioButtons() {
+    document.getElementById('urgent').classList.remove('urgent');
+    document.getElementById('medium').classList.remove('medium');
+    document.getElementById('low').classList.remove('low');
+}
+
+
+function urgent() {
+    document.getElementById('urgent').classList.add('urgent');
+    document.getElementById('medium').classList.remove('medium');
+    document.getElementById('low').classList.remove('low');
+}
+
+
+function medium() {
+    document.getElementById('medium').classList.add('medium');
+    document.getElementById('urgent').classList.remove('urgent');
+    document.getElementById('low').classList.remove('low');
+}
+
+
+function low() {
+    document.getElementById('low').classList.add('low');
+    document.getElementById('medium').classList.remove('medium');
+    document.getElementById('urgent').classList.remove('urgent');
 }
