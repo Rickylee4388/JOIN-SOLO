9de8ae4022538a8f568d3299a4b3d85a790a0478
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-06-02 13:30:43 +0200

    little improvements to add task page

--- templates/js/addTask.js
+++ templates/js/addTask.js
@@ -1,3 +1,26 @@
+let allTasks = [];
+
+
+function createTask() {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let category = document.getElementById('category').value;
+    let assignedTo = document.getElementById('assignedTo').value;
+    let date = document.getElementById('date').value;
+
+    let newTask = {
+        'title': title,
+        'description': description,
+        'category': category,
+        'assignedTo': assignedTo,
+        'date': date
+    };
+
+    allTasks.push(newTask);
+    clearFields();
+}
+
+
 function clearFields() {
     document.getElementById('title').value = '';
     document.getElementById('description').value = '';
@@ -11,25 +34,44 @@ function clearPrioButtons() {
     document.getElementById('urgent').classList.remove('urgent');
     document.getElementById('medium').classList.remove('medium');
     document.getElementById('low').classList.remove('low');
+
+    document.getElementById('urgentIcon').src = '../../img/urgentIcon.png';
+    document.getElementById('mediumIcon').src = '../../img/mediumIcon.png';
+    document.getElementById('lowIcon').src = '../../img/lowIcon.png';
 }
 
 
 function urgent() {
     document.getElementById('urgent').classList.add('urgent');
+    document.getElementById('urgentIcon').src = '../../img/urgentWhiteIcon.png';
+
     document.getElementById('medium').classList.remove('medium');
+    document.getElementById('mediumIcon').src = '../../img/mediumIcon.png';
+
     document.getElementById('low').classList.remove('low');
+    document.getElementById('lowIcon').src = '../../img/lowIcon.png';
 }
 
 
 function medium() {
     document.getElementById('medium').classList.add('medium');
+    document.getElementById('mediumIcon').src = '../../img/mediumWhiteIcon.png';
+
     document.getElementById('urgent').classList.remove('urgent');
+    document.getElementById('urgentIcon').src = '../../img/urgentIcon.png';
+
     document.getElementById('low').classList.remove('low');
+    document.getElementById('lowIcon').src = '../../img/lowIcon.png';
 }
 
 
 function low() {
     document.getElementById('low').classList.add('low');
+    document.getElementById('lowIcon').src = '../../img/lowWhiteIcon.png';
+
     document.getElementById('medium').classList.remove('medium');
+    document.getElementById('mediumIcon').src = '../../img/mediumIcon.png';
+
     document.getElementById('urgent').classList.remove('urgent');
+    document.getElementById('urgentIcon').src = '../../img/urgentIcon.png';
 }
