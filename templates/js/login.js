2029e3317b27afc4a5766049cc863b562b038ab7
Denise Schmidt <rickylee4388@web.de>
2023-06-05 13:56:36 +0200

    signUp&Login connect

--- templates/js/login.js
+++ templates/js/login.js
@@ -3,10 +3,41 @@ function getMsg() {
   const msg = urlParams.get("msg");
 
   if (msg) {
+    //Test overlay nachricht//
+
+    // document.getElementById('overlaySection').innerHTML = `<img class="overlayAddContactSuccess" src="/../img/newContactSuccess.svg">`;
+    // document.getElementById('overlaySection').classList.remove('d-none')
+    //   setTimeout(function () {
+    //       document.getElementById('overlaySection').classList.add('d-none');
+    //   }, 1400);
+    // }
+
     document.getElementById("msgBox").innerHTML = `${msg}`;
-  }
-  else{
-    document.getElementById("msgBox").classList.add(d-none);
+    document.getElementById("msgBoxDiv").classList.remove("d-none");
+  } else {
+    document.getElementById("msgBoxDiv").classList.remove("d-flex");
   }
 }
-function login() {}
+
+function leadToSignUp() {
+  window.location.href = "signUp.html";
+}
+function guestLogIn() {
+  window.location.href = "summary.html";
+}
+
+function login() {
+  let email = document.getElementById("loginEmail");
+  let password = document.getElementById("loginPassword");
+  let user = userLogin.find(
+    (u) => u.email == email.value && u.password == password.value
+  );
+
+  console.log(user);
+  if (user) {
+    console.log("user gefunden");
+    window.location.href = "summary.html";
+  } else {
+    alert("Email oder Passwort nicht korrekt!");
+  }
+}
---
+++ templates/js/signUp.js
@@ -0,0 +1,28 @@
+let test123 = [];
+
+let userLogin = [];
+async function loadUserLogin() {
+  let users = JSON.parse(await getItem("userLogin"));
+  userLogin = users;
+}
+async function signUp() {
+  signUpbtn.disabled = true;
+  userLogin.push({
+    name: signUpName.value,
+    email: signUpEmail.value,
+    password: signUpPassword.value,
+  });
+
+  setItem("userLogin", JSON.stringify(userLogin));
+  resetForm();
+  window.location.href = "login.html?msg=Du hast dich erfolgreich registriert";
+}
+function backToLogin() {
+  window.location.href = "login.html";
+}
+function resetForm() {
+  signUpName.value = "";
+  signUpEmail.value = "";
+  signUpPassword.value = "";
+  signUpbtn.disabled = false;
+}
