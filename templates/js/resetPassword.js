30ccbb2fc61219699b706074b8ded24fa83fef18
Denise Schmidt <rickylee4388@web.de>
2023-06-10 12:32:33 +0200

    animation login

---
+++ templates/js/resetPassword.js
@@ -0,0 +1,6 @@
+function popUpMessageEmail() {
+document.getElementById('buttonAnimationEmail').classList.remove('d-none');
+}
+function popUpMessagePw() {
+    document.getElementById('buttonAnimationPw').classList.remove('d-none');
+    }
