b8cce1010227956e98e0e0a7ed95529de59985ce
Denise Schmidt <rickylee4388@web.de>
2023-09-17 13:11:26 +0200

    margin summary and scrollbar

--- templates/js/addTask1.js
+++ templates/js/addTask1.js
@@ -60,7 +60,7 @@ function renderContactsAddTask(Id) {
         const allData = allContacts[i];
         const { name, color } = getJoinData(allData);
         document.getElementById(Id).innerHTML += /*html*/ `
-            <option value="${color}">${name}</option>
+            <option id="${color}" value="${name}">${name}</option>
         `;
     }
 }
